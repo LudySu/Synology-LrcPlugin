02dc3e0281f1a66f0c4e25d2527f469482f615b2
Ludy Su <ludy.su@gmail.com>
2017-11-10 11:32:26 +1300

    Lyric translation: finished

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
-!#/bin/bash
+#!/bin/bash
 
-tar czf netease.php
+# Disable debug
+sed -i.bak  "s@^const.DEBUG.*;@const DEBUG = false;@" netease.php
+
+# Generate the no Chinsese translation version
+sed -i.bak "s@^const.NEED_TRANSLATION.*;@const NEED_TRANSLATION = false;@" netease.php
+tar czf netease_org.aum netease.php INFO
+
+# Generate the with Chinsese translation version
+sed -i.bak "s@^const.NEED_TRANSLATION.*;@const NEED_TRANSLATION = true;@" netease.php
+tar czf netease_trans.aum netease.php INFO
+
+# Clean up
+rm netease.php.bak
+git checkout netease.php
